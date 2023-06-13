2052dc29fcc7e6eb36ce34a4429aaa4dfd9c9bd3
kash-patel <ksp.sdd.ptl@gmail.com>
2023-06-13 19:31:24 -0400

    UI tweaks

--- app/src/main/java/com/kashithekash/violinesque/ui/theme/Theme.kt
+++ app/src/main/java/com/kashithekash/violinesque/ui/theme/Theme.kt
@@ -1,14 +1,20 @@
 package com.kashithekash.violinesque.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 
 private val LightColors = lightColorScheme(
@@ -81,14 +87,16 @@ fun ViolinEsqueTheme(
   useDarkTheme: Boolean = isSystemInDarkTheme(),
   content: @Composable() () -> Unit
 ) {
-//    val context = LocalContext.current
-    val colors = when {
-//        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
-//            if (useDarkTheme) dynamicDarkColorScheme(context)
-//            else dynamicLightColorScheme(context)
-//        }
-        useDarkTheme -> DarkColors
-        else -> LightColors
+
+    val colors = if (useDarkTheme) DarkColors else LightColors
+    val view = LocalView.current
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !useDarkTheme
+        }
     }
 
     MaterialTheme(
